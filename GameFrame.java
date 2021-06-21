1da7b2557df6452bcdb39dbe003825bdb5e2f87c
Far Tayeb <chefredr@gmail.com>
2021-06-20 21:11:04 -0500

    Added buttons and their coords

--- GameFrame.java
+++ GameFrame.java
@@ -1,21 +1,38 @@
 import java.awt.Dimension;
+import java.awt.Font;
+import java.util.ArrayList;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GameFrame extends JFrame {
 
     private JPanel panel = new JPanel();
-    private final int PANEL_WIDTH = 600;
-    private final int PANEL_HEIGHT = 600;
+    private final int PANEL_LENGTH = 600;
+    private final int TILE_SIZE = PANEL_LENGTH/4;
+    private JButton[] tilePieces = new JButton[16];
     
     public GameFrame() {
         setTitle("15 Puzzle");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-        
+        panel.setPreferredSize(new Dimension(PANEL_LENGTH, PANEL_LENGTH));
+        panel.setLayout(null);
+
+        //initialize tiles
+        for(int i=0; i<tilePieces.length-1; ++i) {
+            tilePieces[i] = new JButton();
+            tilePieces[i].setFocusable(false);
+            tilePieces[i].setFont(new Font("Ariel", Font.BOLD, 75));
+            String randomInt = String.valueOf((int) (Math.random() * 15));
+            tilePieces[i].setText(randomInt);
+            tilePieces[i].setBounds(i%4 * TILE_SIZE, ((i*TILE_SIZE)/PANEL_LENGTH)*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            panel.add(tilePieces[i]);
+        }
+
         add(panel);
         pack();
         setVisible(true);
     }
+
 }
