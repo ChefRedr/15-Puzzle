d5f49863e7da461a0c4f9f831e523606c75ba69c
Far Tayeb <chefredr@gmail.com>
2021-06-21 00:38:40 -0500

    Set the board

--- GameFrame.java
+++ GameFrame.java
@@ -19,13 +19,13 @@ public class GameFrame extends JFrame {
         panel.setLayout(null);
 
         //initialize tiles
-        for(int i=0; i<tilePieces.length-1; ++i) {
+        for(int i=0; i<tilePieces.length; ++i) {
             tilePieces[i] = new JButton();
             tilePieces[i].setFocusable(false);
             tilePieces[i].setFont(new Font("Ariel", Font.BOLD, 75));
-            String randomInt = String.valueOf((int) (Math.random() * 15));
-            tilePieces[i].setText(randomInt);
             tilePieces[i].setBounds(i%4 * TILE_SIZE, ((i*TILE_SIZE)/PANEL_LENGTH)*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+            if(i == 15) { tilePieces[i].setEnabled(false); }
+            else { tilePieces[i].setText(""+(i+1)); }
             panel.add(tilePieces[i]);
         }
 
